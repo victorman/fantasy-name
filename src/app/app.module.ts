646bafb8e98036debd58f6a64fc38e1b93401b56
Victor Frandsen <vfrandsen@aurtech.local>
2018-12-19 16:37:21 -0800

    Added Male/Female/Neutral name differentiation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NameGeneratorComponent } from './name-generator/name-generator.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameGeneratorComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/names.model.ts
@@ -0,0 +1,39 @@
+export class Names {
+  public static maleFirstNames: string[] = [
+    'Alfred',
+    'Ned',
+    'Conan',
+    'Merlin',
+    'Edwin',
+    'Drizzt',
+    'Drax'
+  ];
+  public static maleLastNames: string[] = [
+    'Din-Maker',
+    'Hammer-Din',
+    'The Anvil',
+    'Strongjaw',
+    'Stout-drought'
+  ];
+  public static femaleFirstNames: string[] = [
+    'Ursila',
+    'Yvonne',
+    'Aurora',
+    'Jezabelle',
+    'Ingred'
+  ];
+  public static femaleLastNames: string[] = [
+    'The Seductress',
+    'The Eternal Beauty',
+    'The Enchantress',
+    'Duchess of Pain',
+  ];
+  public static neutralLastNames: string[] = [
+    'The Terrible',
+    'Dark-Blade',
+    'Plague-Tongue',
+    'The Savage',
+    'The Destroyer',
+    'Man-Slayer'
+  ];
+}
