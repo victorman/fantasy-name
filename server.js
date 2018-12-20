f0d37da756ef6d6d7bd6657015ef7b56486cb035
Victor <mrweektor@gmail.com>
2018-12-19 19:54:02 -0800

    another change to server.js

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.listen(process.env.PORT || 5000);
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 
-app.request.get('/*', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
